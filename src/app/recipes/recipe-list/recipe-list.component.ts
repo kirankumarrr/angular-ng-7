d938051c9022bf96c535f9a9f9537f11ece9103a
kirankumarrr <kirankumarrrreddy@gmail.com>
2019-03-20 12:10:12 +0530

    5 files modified, 2 added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe } from "../recipe.model";
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [ 
+    new Recipe('Kiran','Working', 'https://image.shutterstock.com/image-illustration/frontend-development-chart-keywords-icons-450w-491050822.jpg'),
+     new Recipe('Kiran','Working', 'https://image.shutterstock.com/image-illustration/frontend-development-chart-keywords-icons-450w-491050822.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
+    debugger;
+    console.log(this.arr);
   }
 
 }
